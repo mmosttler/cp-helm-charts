42170ab0414326cf837f5e16e164538269f9f00e
Marcus Mosttler <mmosttler@harryanddavid.com>
2019-03-26 19:33:41 +0000

    schema registry keystore

--- certs.sh
+++ certs.sh
@@ -45,4 +45,34 @@ keytool -keystore certs/kafka.server.keystore.jks -alias broker -importkeystore 
 keytool -keystore certs/kafka.server.keystore.jks -alias CARoot -import -file certs/ca-cert -storepass $BROKERSTOREPASS -noprompt
 
 
-keytool -keystore certs/schema.registry.keystore.jks -alias schema-registry.kafka-test.domain.com -importkeystore -srckeystore certs/keystore.p12 -srcstorepass temp -deststorepass $BROKERSTOREPASS -destkeypass $KEYPASS -noprompt
+# SCHEMA REGISTRY
+SCHEMA_SERVICE=confluent-cp-schema-registry
+SCHEMA_CN=schema-registry.kafka-test.domain.com
+
+cat << EOF > certs/schema-registry-extensions.cnf
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = $SCHEMA_SERVICE
+DNS.2 = *.$SCHEMA_SERVICE
+DNS.3 = *.$SCHEMA_SERVICE.$NAMESPACE
+DNS.4 = *.$SCHEMA_SERVICE.$NAMESPACE.svc.$KUBERNETES_SERVICE_DNS_DOMAIN
+DNS.5 = $SCHEMA_CN
+DNS.6 = *.$SCHEMA_CN
+EOF
+
+openssl req -new -x509 -keyout certs/schema-registry-ca-key -out certs/schema-registry-ca-cert -days $VALIDITY -passout pass:$CAPASS -subj "/CN=$SCHEMA_CN"
+
+
+openssl req -new -newkey rsa:2048 -sha256 -keyout certs/cert-key -out certs/cert-req -config certs/schema-registry-extensions.cnf -passin pass:$KEYPASS -passout pass:$KEYPASS -subj "/CN=$SCHEMA_CN"
+
+openssl x509 -req -in certs/schema-registry-cert-req -CA certs/schema-registry-ca-cert -CAkey certs/schema-registry-ca-key -CAcreateserial -out certs/schema-registry-signed-cert.pem -extensions v3_req -extfile certs/schema-registry-extensions.cnf -passin pass:$SCHEMA_CN
+
+openssl pkcs12 -export -name broker -in certs/schema-registry-signed-cert.pem -inkey certs/schema-registry-cert-key -out certs/schema-registry-keystore.p12 -passin pass:$KEYPASS -password pass:temp
+
+
+keytool -keystore certs/schema.registry.keystore.jks -alias $SCHEMA_CN -importkeystore -srckeystore certs/schema-registry-keystore.p12 -srcstorepass temp -deststorepass $BROKERSTOREPASS -destkeypass $KEYPASS -noprompt
